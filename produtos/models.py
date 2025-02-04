40bd047a1983fe35b3bc331e0860c24ffb7834fd
kaique <moraiskaique06@gmail.com>
2025-02-04 16:39:45 -0300

    terminando models

--- produtos/models.py
+++ produtos/models.py
@@ -3,7 +3,7 @@ from django.utils.timezone import now
 
 class Produto(models.Model):
     nome_do_produto = models.CharField(max_length=150)
-    descricao = models.CharField(max_length=3000)
+    descricao = models.TextField(max_length=3000)
     estoque = models.IntegerField()
     preco = models.DecimalField(decimal_places=2, max_digits=10)
     novidade = models.BooleanField()
@@ -14,7 +14,7 @@ class Produto(models.Model):
     
 class Encomenda(models.Model):
     nome_da_pessoa = models.CharField(max_length=100)
-    Produto_reservado = models.CharField(max_length=3000)
+    Produto_reservado = models.TextField(max_length=3000)
     numero_de_telefone = models.CharField(max_length=15)
     total = models.DecimalField(decimal_places=2, max_digits=10, default=0)
     endereço_da_entrega = models.CharField(max_length=100)
